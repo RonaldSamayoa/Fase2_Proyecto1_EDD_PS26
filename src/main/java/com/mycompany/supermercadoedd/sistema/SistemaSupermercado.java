27c03501e200cba2f339dce4909903fdaa6e30f3
RonaldSamayoa <RonaldSamayoa@github.com>
2026-04-29 23:58:00 -0600

    implementacion de traslado de productos

--- src/main/java/com/mycompany/supermercadoedd/sistema/SistemaSupermercado.java
+++ src/main/java/com/mycompany/supermercadoedd/sistema/SistemaSupermercado.java
@@ -11,24 +11,26 @@ import com.mycompany.supermercadoedd.modelos.Producto;
 public class SistemaSupermercado {
     // Almacena todas las sucursales registradas
     private ListaEnlazada<Sucursal> sucursales;
+    private GrafoSucursales grafo;
 
     // Inicializa el sistema principal
     public SistemaSupermercado() {
-
         // Se crea la lista principal de sucursales
         sucursales = new ListaEnlazada<>();
+        
+        // Se inicializa el grafo de conexiones
+        grafo = new GrafoSucursales();
     }
 
     // Registra una nueva sucursal en el sistema
     public void agregarSucursal(Sucursal sucursal) {
-
         // Inserta la sucursal al final de la lista
         sucursales.insertarAlFinal(sucursal);
+        grafo.agregarSucursal(sucursal); //agregar dentro del grafo
     }
 
     // Busca una sucursal usando su id
     public Sucursal buscarSucursalPorId(int id) {
-
         // Recorre toda la lista de sucursales
         for (int i = 0; i < sucursales.obtenerTamanio(); i++) {
 
@@ -46,7 +48,6 @@ public class SistemaSupermercado {
 
     // Registra un producto dentro de su sucursal correspondiente
     public boolean registrarProducto(int idSucursal, Producto producto) {
-
         // Busca la sucursal destino
         Sucursal sucursal = buscarSucursalPorId(idSucursal);
 
@@ -59,7 +60,6 @@ public class SistemaSupermercado {
 
     // Muestra todas las sucursales registradas
     public void mostrarSucursales() {
-
         for (int i = 0; i < sucursales.obtenerTamanio(); i++) {
             System.out.println(
                     sucursales.obtener(i)
@@ -70,5 +70,75 @@ public class SistemaSupermercado {
     // Retorna la lista completa de sucursales
     public ListaEnlazada<Sucursal> getSucursales() {
         return sucursales;
+    }
+    
+    // Traslada cierta cantidad de un producto
+    // desde una sucursal origen hacia una sucursal destino
+    public boolean trasladarProducto(int idOrigen, int idDestino,
+            String codigoBarras, int cantidad) {
+
+        // Busca ambas sucursales
+        Sucursal origen = buscarSucursalPorId(idOrigen);
+
+        Sucursal destino = buscarSucursalPorId(idDestino);
+
+        // Si alguna no existe, falla
+        if (origen == null || destino == null) {
+            return false;
+        }
+
+        // Busca el producto en la sucursal origen
+        Producto productoOrigen = origen.buscarProductoPorCodigo(codigoBarras);
+
+        // Si no existe, falla
+        if (productoOrigen == null) {
+            return false;
+        }
+
+        // Verifica stock suficiente
+        if (productoOrigen.getStock() < cantidad) {
+            return false;
+        }
+
+        // Calcula la ruta mínima usando Dijkstra
+        int distancia =
+                grafo.dijkstra(idOrigen,idDestino);
+
+        // Si no existe ruta posible, falla
+        if (distancia == -1) {
+            return false;
+        }
+
+        // Descuenta stock en origen
+        boolean descuento =
+                origen.disminuirStock(codigoBarras,cantidad);
+
+        if (!descuento) {
+            return false;
+        }
+
+        // Intenta aumentar en destino
+        boolean aumento = destino.aumentarStock(codigoBarras,cantidad);
+
+        // Si no existe el producto en destino,
+        // se crea una copia e inserta
+        if (!aumento) {
+            Producto nuevoProducto =
+                    new Producto(
+                            productoOrigen.getNombre(),
+                            productoOrigen.getCodigoBarras(),
+                            productoOrigen.getCategoria(),
+                            productoOrigen.getFechaCaducidad(),
+                            productoOrigen.getMarca(),
+                            productoOrigen.getPrecio(),
+                            cantidad
+                    );
+            destino.agregarProducto(nuevoProducto);
+        }
+
+        System.out.println("Traslado realizado correctamente. " +
+                "Tiempo mínimo estimado: " + distancia
+        );
+        return true;
     }
 }
